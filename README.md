541398e4421504e1e0fbd9b991271f1b4edb4815
Gess1t <tintamare91@gmail.com>
2024-10-03 22:08:26 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Relative Mode Area
+
+This plugin will allow you to define area within which you need need to place your finger to move the cursor.
+
+## Dependencies
+
+- [OpenTabletDriver 0.6.4.0](https://github.com/OpenTabletDriver/OpenTabletDriver)
+- [OTD.EnhancedOutputMode](https://github.com/Mrcubix/OTD.EnhancedOutputMode)
+
+## Installation
+
+Place the dll for this plugin in the same folder as OTD.EnhancedOutputMode.Lib.dll
+(in the same folder where OpenTabletDriver Installed OTD.EnhancedOutputMode).
---
+++ src/RelativeModeArea.cs
@@ -0,0 +1,177 @@
+﻿using System.Drawing;
+using System.Numerics;
+using OpenTabletDriver;
+using OpenTabletDriver.Plugin;
+using OpenTabletDriver.Plugin.Attributes;
+using OpenTabletDriver.Plugin.DependencyInjection;
+using OpenTabletDriver.Plugin.Output;
+using OpenTabletDriver.Plugin.Tablet;
+using OTD.EnhancedOutputMode.Lib.Tablet;
+
+namespace RelativeModeArea;
+
+[PluginName(PLUGIN_NAME)]
+public class RelativeModeArea : IPositionedPipelineElement<IDeviceReport>
+{
+    public const string PLUGIN_NAME = "Relative Mode Area";
+
+    private TabletReference _tablet;
+    private IDriver _driver;
+    private bool _initialized = false;
+
+    private Vector2 _penLpmm;
+    private Vector2 _touchLpmm;
+    private RectangleF _penRect;
+    private RectangleF _touchRect;
+
+    public PipelinePosition Position => PipelinePosition.PreTransform;
+
+    public event Action<IDeviceReport> Emit;
+
+    [TabletReference]
+    public TabletReference Tablet { 
+        get => _tablet;
+        set
+        {
+            _tablet = value;
+            Initialize(_tablet, _driver);
+        }
+    }
+
+    [Resolved]
+    public IDriver Driver
+    {
+        get => _driver;
+        set
+        {
+            _driver = value;
+            Initialize(_tablet, _driver);
+        }
+    }
+
+    public void Initialize(TabletReference tablet, IDriver driver)
+    {
+        _tablet = tablet;
+        _driver = driver;
+
+        // Obtain the output Mode
+        if (_tablet == null || _driver == null)
+            return;
+
+        if (_driver is not Driver oDriver)
+            return;
+
+        var deviceTree = oDriver.InputDevices.Where(x => x.Properties == Tablet.Properties)
+                                             .Where(x => x.OutputMode is RelativeOutputMode)
+                                             .FirstOrDefault();
+
+        if (deviceTree == null)
+        {
+            Log.Write(PLUGIN_NAME, "Could not find relative mode tablet", LogLevel.Error);
+            return;
+        }
+
+        var digitizer = _tablet.Properties.Specifications.Digitizer;
+
+        // Lines per millimeter
+        _penLpmm = new Vector2(digitizer.MaxX / digitizer.Width, digitizer.MaxY / digitizer.Height);
+        _touchLpmm = new Vector2(TouchMaxX / digitizer.Width, TouchMaxX / digitizer.Height);
+
+        var topLeft = new Vector2(X, Y) - new Vector2(Width / 2, Height / 2);
+
+        // Rectangles
+        _penRect = new RectangleF(topLeft.X * _penLpmm.X, topLeft.Y * _penLpmm.Y, Width * _penLpmm.X, Height * _penLpmm.Y);
+        _touchRect = new RectangleF(topLeft.X * _touchLpmm.X, topLeft.Y * _touchLpmm.Y, Width * _touchLpmm.X, Height * _touchLpmm.Y);
+
+        _initialized = true;
+    }
+
+    public void Consume(IDeviceReport report)
+    {
+        if (_initialized)
+        {
+            if (report is TouchConvertedReport touchConverted)
+                HandleTouch(touchConverted);
+            else if (report is IAbsolutePositionReport tablet)
+                HandlePosition(tablet);
+        }
+        else
+            Emit?.Invoke(report);
+    }
+
+    private void HandlePosition(IAbsolutePositionReport positionReport)
+    {
+        var position = positionReport.Position;
+
+        if (position.X < _penRect.Left)
+            return;
+        else if (position.X > _penRect.Right)
+            return;
+
+        if (position.Y < _penRect.Top)
+            return;
+        else if (position.Y > _penRect.Bottom)
+            return;
+
+        Emit?.Invoke(positionReport);
+    }
+
+    private void HandleTouch(TouchConvertedReport touchReport)
+    {
+        var position = touchReport.Position;
+
+        if (position.X < _touchRect.Left)
+            return;
+        else if (position.X > _touchRect.Right)
+            return;
+
+        if (position.Y < _touchRect.Top)
+            return;
+        else if (position.Y > _touchRect.Bottom)
+            return;
+
+        Emit?.Invoke(touchReport);
+    }
+
+    [Property("Touch Max X"),
+     DefaultPropertyValue(4095),
+     ToolTip("Relative Mode Area:\n\n" +
+             "The maximum value of the X axis. \n" +
+             "Check the debugger to obtain this value.")]
+    public int TouchMaxX { get; set; }
+
+    [Property("Touch Max Y"),
+     DefaultPropertyValue(4095),
+     ToolTip("Relative Mode Area:\n\n" +
+             "The maximum value of the Y axis. \n" +
+             "Check the debugger to obtain this value.")]
+    public int TouchMaxY { get; set; }
+
+    [Property("Width"),
+     DefaultPropertyValue(1),
+     ToolTip("Relative Mode Area:\n\n" +
+             "The width of the relative mode area. \n" +
+             "Switch to Absolute Mode, create your area and copy the value to here.")]
+    public float Width { get; set; }
+
+    [Property("Height"),
+     DefaultPropertyValue(1),
+     ToolTip("Relative Mode Area:\n\n" +
+             "The height of the relative mode area. \n" +
+             "Switch to Absolute Mode, create your area and copy the value to here.")]
+    public float Height { get; set; }
+
+    [Property("X"),
+     DefaultPropertyValue(1),
+     ToolTip("Relative Mode Area:\n\n" +
+             "The centered position on the X axis. \n" +
+             "Switch to Absolute Mode, create your area and copy the value to here.")]
+    public float X { get; set; }
+
+    [Property("Y"),
+     DefaultPropertyValue(1),
+     ToolTip("Relative Mode Area:\n\n" +
+             "The centered position on the Y axis. \n" +
+             "Switch to Absolute Mode, create your area and copy the value to here.")]
+    public float Y { get; set; }
+}
